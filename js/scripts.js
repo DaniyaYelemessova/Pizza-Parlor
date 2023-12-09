315721ed2abb30fb69881c20008db9b91f37fa74
Daniya Yelemessova <daniya@collingsmail.com>
2023-12-08 21:10:56 -0500

    Add basic display of input information

--- js/scripts.js
+++ js/scripts.js
@@ -13,35 +13,42 @@ Pizza.prototype.finalPrice = function () {
       basePrice += 4;
     } else if (topping === "artichoke") {
       basePrice += 3;
-    }else if(topping === "no topping"){
-      basePrice += 0
+    } else if (topping === "no topping") {
+      basePrice += 0;
     }
   }
 
   if (this.size === "medium") {
     basePrice += 5;
-  }else if(this.size === "large"){
-    basePrice += 10
-  }else{
-    basePrice
+  } else if (this.size === "large") {
+    basePrice += 10;
+  } else {
+    basePrice;
   }
 
   return basePrice;
 };
 
-
-function handlePizzaForm(e){
+function handlePizzaForm(e) {
   e.preventDefault();
   const orderName = document.getElementById("name").value;
   const sizeSelection = document.querySelector("input[name='size']:checked").value;
-  const toppingsSelection = document.querySelectorAll("input:checked");
-  const toppingsSelectionArray = Array.from(toppingsSelection).map(checkbox => checkbox.value);
+  const toppingsSelection = document.querySelectorAll("input[name=topping]:checked");
+  const toppingsSelectionArray = Array.from(toppingsSelection).map(
+    (topping) => topping.value
+  );
   const newPizza = new Pizza(toppingsSelectionArray, sizeSelection);
-  console.log(newPizza.finalPrice(newPizza))
-};
 
+  const welcomeName = document.querySelector("#welcome");
+  welcomeName.textContent = `Welcome, ${orderName}! Happy to see you today!`;
 
-document.addEventListener("DOMContentLoaded", function(){
+  const sizeOutput = document.querySelector("#sizeOutput");
+  sizeOutput.textContent = `Your size: ${sizeSelection}.`;
+  const toppingsOutput = document.querySelector("#toppingsOutput");
+  toppingsOutput.textContent = `Toppings: ${toppingsSelectionArray.join(" , ")}`
+}
+
+document.addEventListener("DOMContentLoaded", function () {
   const form = document.querySelector("#form");
-  form.addEventListener("submit", handlePizzaForm)
-})
+  form.addEventListener("submit", handlePizzaForm);
+});
