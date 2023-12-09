52e4e16fc0bfe03bf79972c6268737f594426b02
Daniya Yelemessova <daniya@collingsmail.com>
2023-12-08 19:22:24 -0500

    Add another topping

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,12 @@ function Pizza(toppings, size) {
 
 Pizza.prototype.finalPrice = function(){
   let basePrice = 30;
-  if(this.toppings === "pepperoni"){
+  if(this.toppings === "pepperoni" || this.toppings === "meatballs"){
     basePrice += 7
   }
   return basePrice
 }
 
-const myPizza = new Pizza("no topping", "medium");
+const myPizza = new Pizza("meatballs", "medium");
 
 console.log(myPizza.finalPrice())
